3f643b8b14528a29d358a42251589594f99d921a
praveen-221 <praveen-221>
2023-03-18 13:43:32 +0530

    Initial Version 1.0

--- batscript/__init__.py
+++ batscript/__init__.py
@@ -3,7 +3,7 @@ from . import ast, lexer, parser, runner, utils
 
 __all__ = ["parser", "ast", "lexer", "runner", "utils"]
 
-__version__ = "0.1"
+__version__ = "1.0"
 __version_str__ = (
     f"BatScript v{__version__}."
     + "\nBatScript is a programming language based on the dialogues of Batman Triology."
